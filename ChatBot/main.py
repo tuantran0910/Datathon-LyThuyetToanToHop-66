c237e4a66ee24f3fdf7bfcdb98084616f6f7b847
chimlun0910 <21280058@student.hcmus.edu.vn>
2023-12-16 00:46:25 +0700

    build some simple route for flask app

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -1,10 +1,12 @@
 import os
+import json
+import base64
 import flask_login
 import openai
 import requests
 from .classes import Suggestion
 from dotenv.main import load_dotenv
-from flask import Blueprint, Flask, jsonify, render_template, request
+from flask import Blueprint, Flask, jsonify, render_template, request, send_from_directory
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
@@ -15,18 +17,26 @@ suggestion_list = [
     Suggestion('OP1', 'hi'),
     Suggestion('OP2', 'helo')
 ]
-llm = OpenAI()
+llm = OpenAI(model="text-davinci-003")
 memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=100)
 
 # Blueprint
 main = Blueprint('main', __name__)
 
-# MAiN
-
 
 @main.route('/')
 def index():
     return render_template('base.html')
+
+# @main.route('/print')
+# def print_image(img_urls):
+#     for img_url in img_urls:
+#         # Return json file
+
+#     # Save to static cloth folder and rename xxxxx.jpg
+
+# Aanh -> server upload
+# client : req = anh -> server rec : function upload
 
 
 @main.route('/upload', methods=['POST'])
@@ -39,7 +49,41 @@ def upload_image():
     # Move the file to the 'static' folder
     file.save(os.path.join(os.path.abspath(
         os.path.dirname(__file__)), 'static', file.filename))
+    # Add userid to the file name
     return jsonify({"message": "Upload success", "response": True})
+
+
+@main.route('/getcloth', methods=['POST'])
+def get_cloth():
+    try:
+        urls = json.loads(request.form['urls'])
+        print(urls)
+    except Exception as e:
+        return jsonify({"message": str(e), "response": False})
+    imgs = []
+    for url in urls:
+        with open(url, 'rb') as f:
+            image_data = f.read()
+            base64_data = base64.b64encode(image_data).decode('utf-8')
+            imgs.append(base64_data)
+    return jsonify({"imgs": imgs, "response": True})
+
+
+def get_random_image(num=3):
+    # Get the relative path of 3 random images in static folder
+    static_folder = os.path.join(os.path.dirname(
+        os.path.relpath(__file__)), 'static')
+    img_list = []
+
+    for filename in os.listdir(static_folder):
+        if len(img_list) == num:
+            break
+
+        if filename.endswith('.jpg'):
+            img_list.append(os.path.join(os.path.dirname(
+                os.path.relpath(__file__)), 'static', filename))
+    # Return a list of relative path
+    return img_list
 
 
 @main.route('/data', methods=['POST'])
@@ -47,15 +91,21 @@ def get_data():
     data = request.get_json()
     text = data.get('data')
     user_input = text
+
+    if user_input.strip().lower() == "random anh":
+        imgs = get_random_image()
+        print(imgs)
+        return jsonify({"message": imgs, "list": True, "response": True})
+
     try:
         conversation = ConversationChain(llm=llm, memory=memory)
         output = conversation.predict(input=user_input)
         memory.save_context({"input": user_input}, {"output": output})
-        return jsonify({"response": True, "message": output})
+        return jsonify({"response": True, "list": False, "message": output})
     except Exception as e:
         print(e)
         error_message = f'Error: {str(e)}'
-        return jsonify({"message": error_message, "response": False})
+        return jsonify({"message": error_message, "list": False, "response": False})
 
 
 @main.route("/chatbot")
@@ -81,3 +131,18 @@ def chatbot():
 def profile():
     user = flask_login.current_user
     return render_template('profile.html', user=user)
+
+
+def clear_static_folder():
+    static_folder = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'static')
+    for filename in os.listdir(static_folder):
+        if filename.endswith('.jpg'):
+            file_path = os.path.join(static_folder, filename)
+            os.remove(file_path)
+
+
+@flask_login.login_required
+def test():
+    user = flask_login.current_user
+    user_name = user.name
